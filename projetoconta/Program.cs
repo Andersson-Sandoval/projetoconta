4e6d8715fa5e21c56581e38eecbd7e5eae01da18
Andersson-Sandoval <andersson.sval@hotmail.com>
2022-06-19 16:37:20 -0300

    Adicionar arquivos de projeto.

---
+++ projetoconta/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace projetoconta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+
+            Console.Write("Entre com o Número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o nome do(a) Titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá deposito inicial na conta (S/N)? ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if ( resp == 's' || resp == 'S' ) {
+                Console.Write("Digite o valor de deposito inicial:  ");
+                double depositoInicial = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+              
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+
+            else {
+                conta = new ContaBancaria(numero, titular);    }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine( conta );
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para Saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
